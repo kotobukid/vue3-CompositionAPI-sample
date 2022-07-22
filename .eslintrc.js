ee1665df49762a5401215c3d470c2b9573acfbb3
kotobukid <kotobuki@daqua.jp>
2022-07-22 15:49:46 +0900

    eslintめんどくせえな…

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,11 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'object-curly-spacing': 'off',
+    indent: [
+      'error',
+      2
+    ]
   }
 }
