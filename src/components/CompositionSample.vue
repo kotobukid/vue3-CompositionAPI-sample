a6bd2cabf831b154e71f55ad7bc09564c45d52e0
kotobukid <kotobuki@daqua.jp>
2022-07-22 20:20:33 +0900

    jsxに書き換えようとしたらCompositionAPIの方でエラーが出始めた

--- src/components/CompositionSample.vue
+++ src/components/CompositionSample.vue
@@ -1,30 +1,30 @@
-<template lang="pug">
-.composition_sample
-  span.user {{ user }}
-  input(type="text" v-model="searchQuery")
-  br
-  span {{ searchQuery }}
-  br
-  table(style="width: 200px;")
-    colgroup
-      col(style="width: 100px")
-      col(style="width: 100px")
-    thead
-      tr
-        th 名前
-        th 作成日
-    tbody
-      tr(v-for="r in repositories")
-        td {{ r.name }}
-        td {{ r.created_at }}
-    tbody(v-if="repositories.length === 0")
-      tr
-        td(colspan="2" v-if="repoLoaded") 条件を満たすリポジトリがありません
-        td(colspan="2" v-if="!repoLoaded") リポジトリを読み込んでいます
+<!--<template lang="pug">-->
+<!--.composition_sample-->
+<!--  span.user {{ user }}-->
+<!--  input(type="text" v-model="searchQuery")-->
+<!--  br-->
+<!--  span {{ searchQuery }}-->
+<!--  br-->
+<!--  table(style="width: 200px;")-->
+<!--    colgroup-->
+<!--      col(style="width: 100px")-->
+<!--      col(style="width: 100px")-->
+<!--    thead-->
+<!--      tr-->
+<!--        th 名前-->
+<!--        th 作成日-->
+<!--    tbody-->
+<!--      tr(v-for="r in repositories")-->
+<!--        td {{ r.name }}-->
+<!--        td {{ r.created_at }}-->
+<!--    tbody(v-if="repositories.length === 0")-->
+<!--      tr-->
+<!--        td(colspan="2" v-if="repoLoaded") 条件を満たすリポジトリがありません-->
+<!--        td(colspan="2" v-if="!repoLoaded") リポジトリを読み込んでいます-->
 
-</template>
+<!--</template>-->
 
-<script>
+<script lang="jsx">
 import {toRefs} from 'vue'
 import useUserRepositories from '../composables/useUserRepositories'
 import useRepositoryNameSearch from '../composables/useRepositoriesNameSearch'
@@ -41,15 +41,57 @@ export default {
   setup (props) {
     const { user } = toRefs(props)
 
-    const { repositories, repoLoaded, getUserRepositories } = useUserRepositories(user)
-    const { searchQuery, repositoriesMatchingSearchQuery } = useRepositoryNameSearch(repositories)
+    const { repositories_, repoLoaded } = useUserRepositories(user)
+    const { searchQuery, repositoriesMatchingSearchQuery } = useRepositoryNameSearch(repositories_)
 
-    return {
-      searchQuery,
-      repoLoaded,
-      repositories: repositoriesMatchingSearchQuery,
-      getUserRepositories
-    }
+    // mounted(getUserRepositories)
+
+    // return {
+    //   searchQuery,
+    //   repoLoaded,
+    //   repositories: repositoriesMatchingSearchQuery,
+    //   getUserRepositories
+    // }
+    return () => (
+      <div className="composition_sample">
+        <span className="user">{user}</span>
+        <input type="text" vModel={ searchQuery.value }/>
+        <br/>
+        <span>{searchQuery}</span>
+        <br/>
+        <table style="width: 200px;">
+          <colgroup>
+            <col style="width: 100px"/>
+            <col style="width: 100px"/>
+          </colgroup>
+          <thead>
+            <tr>
+              <th>名前</th>
+              <th>作成日</th>
+            </tr>
+          </thead>
+          <tbody>
+            {repositoriesMatchingSearchQuery.map(r => {
+              return <>
+                <td>{r.name}</td>
+                <td>{r.created_at}</td>
+              </>
+            })}
+          </tbody>
+          {repositoriesMatchingSearchQuery.length === 0 && (
+            <tbody>
+              <tr>
+                {
+                  repoLoaded
+                    ? <td colSpan="2">条件を満たすリポジトリがありません</td>
+                    : <td colSpan="2">リポジトリを読み込んでいます</td>
+                }
+              </tr>
+            </tbody>
+          )}
+        </table>
+      </div>
+    )
   },
   data () {
     return {
--- src/composables/useUserRepositories.js
+++ src/composables/useUserRepositories.js
@@ -1,19 +1,15 @@
-import {ref, onMounted, watch, nextTick} from 'vue'
+import {ref, onMounted, watch} from 'vue'
 
 const fetchUserRepositories = (user) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const repos = [
-        { name: 'hoge repo', created_at: '2020-01-01', owner: 'kotobuki' },
-        { name: 'fuga repo', created_at: '2020-01-02', owner: 'kotobuki' },
-        { name: 'AAA repo', created_at: '2022-01-01', owner: 'manjusai' },
-        { name: 'BBB repo', created_at: '2022-01-02', owner: 'manjusai' }
-      ]
+  const repos = [
+    { name: 'hoge repo', created_at: '2020-01-01', owner: 'kotobuki' },
+    { name: 'fuga repo', created_at: '2020-01-02', owner: 'kotobuki' },
+    { name: 'AAA repo', created_at: '2022-01-01', owner: 'manjusai' },
+    { name: 'BBB repo', created_at: '2022-01-02', owner: 'manjusai' }
+  ]
 
-      resolve(repos.filter(r => {
-        return r.owner === user
-      }))
-    }, 1000)
+  return repos.filter(r => {
+    return r.owner === user
   })
 }
 
@@ -21,19 +17,18 @@ export default function useUserRepositories (user) {
   const repositories = ref([])
   const repoLoaded = ref(false)
 
-  const getUserRepositories = async () => {
+  const getUserRepositories = () => {
     repoLoaded.value = false
     repositories.value = []
 
-    // eslint-disable-next-line vue/valid-next-tick
-    await nextTick(async () => {
-      repositories.value = await fetchUserRepositories(user.value)
-      repoLoaded.value = true
-    })
+    repositories.value = fetchUserRepositories(user.value)
+    repoLoaded.value = true
   }
 
   onMounted(getUserRepositories)
   watch(user, getUserRepositories)
+  console.log(repositories.value)
+  console.log(repoLoaded.value)
 
   return {
     repoLoaded,
